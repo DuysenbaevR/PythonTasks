434d685f632cb2d1fe219051103b453361519a5e
DuysenbaevR <115212950+DuysenbaevR@users.noreply.github.com>
2023-02-01 11:58:57 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+#1
+name = input('Type name :')
+admin = name
+print(f"#1: {admin}")
+
+#2
+first = int(input('firstNumber: '))
+second = int(input('secondNumber: '))
+a1 = first + second
+a2 = first - second
+a3 = first * second
+a4 = first / second
+print(f"#2: a1 = {a1}, a2 = {a2}, a3 = {a3}, a4 = {a4}")
+
+#3
+a6 = 5 % 3
+a7 = 3 % 5
+a8 = 5 * int('3')
+a9 = int('5') - 3
+a10 = str('75') + 'kg'
+print(f"#3: a6 = {a6}, a7 = {a7}, a8 = {a8}, a9 = {a9}, a10 = {a10}")
+
+#4
+height = int(input('Height: '))
+width = int(input('Width: '))
+s = float(height * width)
+print(f"#4 Square = {s}")
+
+
+
